e027cc7e943c8a6b94ff7675554e459a2cc36de7
westorres9 <wester.torres9@gmail.com>
2022-11-11 09:04:16 -0300

    projeto dscommerce criado

---
+++ backend/src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
