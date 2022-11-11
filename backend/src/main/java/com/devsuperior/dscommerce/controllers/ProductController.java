420393adeddbe558e71b3bcf1862ef55dfec0e2a
westorres9 <wester.torres9@gmail.com>
2022-11-11 15:36:49 -0300

    ProductRepository

--- backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping
     public String teste() {
