f951184760693969ce16b159cce639d569744673
westorres9 <wester.torres9@gmail.com>
2022-11-11 15:34:14 -0300

    ProductController

---
+++ backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public String teste() {
+        return "Olá mundo";
+    }
+}
