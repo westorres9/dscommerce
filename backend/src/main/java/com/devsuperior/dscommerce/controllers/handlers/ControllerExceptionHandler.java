4c092cdcaa5dcf8e4180be8681f9b4446f20bb9a
westorres9 <wester.torres9@gmail.com>
2022-11-12 12:02:54 -0300

    ControllerExceptionHandler databaseException

--- backend/src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.controllers.handlers;
 
 import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,17 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError();
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
