e0c162d01fbf49c882be28fc6fe69aab4fdc29c8
westorres9 <wester.torres9@gmail.com>
2022-11-16 15:59:44 -0300

    BCryptPasswordEncoder

--- backend/src/main/java/com/devsuperior/dscommerce/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscommerce/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,5 +15,10 @@ public class WebSecurityConfig {
         http.headers().frameOptions().disable();
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
