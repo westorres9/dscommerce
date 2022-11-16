d07296c009a6dbedc6da78bc9ea7ed6b92da30a0
westorres9 <wester.torres9@gmail.com>
2022-11-16 15:54:48 -0300

    UserRepository, UserService

---
+++ backend/src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscommerce.repositories;
+
+import com.devsuperior.dscommerce.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
---
+++ backend/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+        return user;
+    }
+}
