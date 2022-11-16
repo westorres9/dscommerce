76632d1a1c998214d42c523ab49907e5f2841ce1
westorres9 <wester.torres9@gmail.com>
2022-11-16 15:51:39 -0300

    UserDetails in User Entity

--- backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscommerce.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 
 import java.io.Serializable;
@@ -8,7 +11,7 @@ import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -19,8 +22,6 @@ public class User implements Serializable {
     @Column(unique = true)
     private String email;
     private String phone;
-
-
     private LocalDate birthDate;
     private String password;
 
@@ -113,4 +114,35 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
 }
