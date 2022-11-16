79596f773df3bb3f37fc2190322aa3eadc8f1f0c
westorres9 <wester.torres9@gmail.com>
2022-11-16 15:55:23 -0300

    UserDetailsService

--- backend/src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
