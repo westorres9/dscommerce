6f7cd167dd4559b04456276086e1c999c760b07d
westorres9 <wester.torres9@gmail.com>
2022-11-17 09:43:30 -0300

    OrderDTO

---
+++ backend/src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -0,0 +1,93 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderItem;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    private ClientDTO client;
+
+    private PaymentDTO payment;
+
+    private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public OrderDTO(Order entity) {
+        this.id = entity.getId();
+        this.moment = entity.getMoment();
+        this.status = entity.getStatus();
+        this.client = new ClientDTO(entity.getClient());
+        this.payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public void setClient(ClientDTO client) {
+        this.client = client;
+    }
+
+    public PaymentDTO getPayment() {
+        return payment;
+    }
+
+    public void setPayment(PaymentDTO payment) {
+        this.payment = payment;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        double total = 0;
+        for (OrderItemDTO item : items)  {
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+}
