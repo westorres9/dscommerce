4b4773e0af4f54ee828650d13c4bf6561e3cb018
westorres9 <wester.torres9@gmail.com>
2022-11-17 10:23:28 -0300

    fix ImgUrl in orderItems

--- backend/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ backend/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,15 +8,17 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
     public OrderItemDTO(){
     }
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -24,6 +26,7 @@ public class OrderItemDTO {
         this.name = entity.getProduct().getName();
         this.price = entity.getPrice();
         this.quantity = entity.getQuantity();
+        this.imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -60,5 +63,13 @@ public class OrderItemDTO {
 
     public Double getSubTotal () {
         return price * quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 }
