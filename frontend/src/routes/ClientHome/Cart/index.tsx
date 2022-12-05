a84ae9c509c6ec28db3e51aa856fae0ce7ea8f96
westorres9 <wester.torres9@gmail.com>
2022-12-05 08:38:45 -0300

    cart Mockdata

--- frontend/src/routes/ClientHome/Cart/index.tsx
+++ frontend/src/routes/ClientHome/Cart/index.tsx
@@ -1,43 +1,49 @@
 import './styles.css';
 import computerImg from '../../../assets/img/computer.png'
 
+const cart = {
+  items: [
+  {
+  productId: 4,
+  quantity: 1,
+  name: "PC Gamer",
+  price: 1200,
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"},
+  {
+  productId: 5,
+  quantity: 2,
+  name: "Rails for Dummies",
+  price: 100.99,
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"}
+  ]
+  }
+
 export default function Cart() {
     return (
         <main>
       <section id="cart-container-section" className="dsc-container">
         <div className="dsc-card dsc-mb20">
-          <div className="dsc-cart-item-container dsc-line-bottom">
+
+          {cart.items.map(item => (
+            <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
             <div className="dsc-cart-item-left">
-              <img src={computerImg} alt="Computador" />
+              <img src={item.imgUrl} alt={item.name} />
               <div className="dsc-cart-item-description">
-                <h3>Computador Gamer XT</h3>
+                <h3>{item.name}</h3>
                 <div className="dsc-cart-item-quantity-container">
                   <div className="dsc-cart-item-quantity-btn">-</div>
-                  <p>1</p>
+                  <p>{item.quantity}</p>
                   <div className="dsc-cart-item-quantity-btn">+</div>
                 </div>
               </div>
             </div>
             <div className="dsc-cart-item-right">
-              R$ 5000,00
+              R$ {(item.quantity * item.price).toFixed(2)}
             </div>
           </div>
-          <div className="dsc-cart-item-container dsc-line-bottom">
-            <div className="dsc-cart-item-left">
-              <img src={computerImg}  alt="Computador" />
-              <div className="dsc-cart-item-description">
-                <h3>Computador Gamer XT</h3>
-                <div className="dsc-cart-item-quantity-container">
-                  <div className="dsc-cart-item-quantity-btn">-</div>
-                  <p>1</p>
-                  <div className="dsc-cart-item-quantity-btn">+</div>
-                </div>
-              </div>
-            </div>
-            <div className="dsc-cart-item-right">
-              R$ 5000,00
-            </div>
-          </div>
+          ))}
+          
+          
           <div className="dsc-cart-total-container">
             <h3>R$ 15000,00</h3>
           </div>
