8c39a54fc90181f0e15969abae0d962df3ec945a
westorres9 <wester.torres9@gmail.com>
2022-12-05 09:20:54 -0300

    CART_KEY global variable

--- frontend/src/localStorage/cart-repository.ts
+++ frontend/src/localStorage/cart-repository.ts
@@ -1,11 +1,12 @@
 import { Order } from "../types/order";
+import { CART_KEY } from "../utils/system";
 
 export function save(cart: Order){
     const str = JSON.stringify(cart)
-    localStorage.setItem("com.devsuperior.dscommerce/cart" , str)
+    localStorage.setItem(CART_KEY , str)
 }
 
 export function get() : Order {
-    const str = localStorage.getItem("com.devsuperior.dscommerce/cart") || '{"items"=[]}';
+    const str = localStorage.getItem(CART_KEY) || '{"items"=[]}';
     return JSON.parse(str);
 }
