9c845237254cfff0c049525a048b22cd802b323f
westorres9 <wester.torres9@gmail.com>
2022-12-07 09:02:46 -0300

    Test hasAnyRoles

--- frontend/src/services/auth-service.ts
+++ frontend/src/services/auth-service.ts
@@ -1,4 +1,4 @@
-import { AccessTokenPayload, Credentials } from "../types/auth";
+import {AccessTokenPayload, Credentials, RoleEnum} from "../types/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import QueryString from "qs";
 import { AxiosRequestConfig } from "axios";
@@ -53,4 +53,22 @@ export function getAccessTokenPayload(): AccessTokenPayload | undefined {
 export function isAuthenticated(): boolean {
     let tokenPayload = getAccessTokenPayload();
     return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+    if (roles.length === 0) {
+        return true;
+    }
+
+    const tokenPayload = getAccessTokenPayload();
+
+    if (tokenPayload !== undefined) {
+        for (var i = 0; i < roles.length; i++) {
+            if (tokenPayload.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+    //return roles.some(role => tokenData.authorities.includes(role));
+    }
+    return false;
 }
