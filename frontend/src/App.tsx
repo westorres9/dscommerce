55e98cf2cbf94c679f3b9a667a26b2c10c7bc9d8
westorres9 <wester.torres9@gmail.com>
2022-12-07 11:25:42 -0300

    loginAndAccessControl Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,8 @@ import {ContextToken} from "./utils/context-token";
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
 import Confirmation from "./routes/ClientHome/Confirmation";
+import ProductListing from "./routes/Admin/ProductListing";
+import ProductForm from "./routes/Admin/ProductForm";
 
 
 export default function App() {
@@ -46,7 +48,10 @@ export default function App() {
                             <Route path="login" element={<Login />} />
                         </Route>
                         <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin/></PrivateRoute>}>
-                            <Route index element={<AdminHome />} />
+                            <Route index element={<Navigate to="/admin/home"/>} />
+                            <Route path="home" element={<AdminHome />} />
+                            <Route path="products" element={<ProductListing />}/>
+                            <Route path="products/:productId" element={<ProductForm />}/>
                         </Route>
                         <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
