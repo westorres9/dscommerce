0eb05222b65bb4d8d70f211398b0e557ae155156
westorres9 <wester.torres9@gmail.com>
2022-12-07 11:46:36 -0300

    layout product forms page

--- frontend/src/routes/Admin/ProductForm/index.tsx
+++ frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,5 +1,41 @@
+import './styles.css';
+
 export default function ProductForm() {
     return (
-    <h1>Formulario de produtos</h1>
+        <main>
+            <section id="product-form-section" className="dsc-container">
+                <div className="dsc-product-form-container">
+                    <form className="dsc-card dsc-form">
+                        <h2>Dados do produto</h2>
+                        <div className="dsc-form-controls-container">
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Nome"/>
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Preço"/>
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Imagem"/>
+                            </div>
+                            <div>
+                                <select className="dsc-form-control dsc-select" required>
+                                    <option value="" disabled selected>Categorias</option>
+                                    <option value="1">Valor 1</option>
+                                    <option value="2">Valor 2</option>
+                                </select>
+                            </div>
+                            <div>
+                                <textarea className="dsc-form-control dsc-textarea" placeholder="Descrição"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="dsc-product-form-buttons">
+                            <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
     );
 }
