8bdd86828db8e470397a533a0c54ba6fb823321a
westorres9 <wester.torres9@gmail.com>
2022-12-08 08:48:25 -0300

    forms to values

--- frontend/src/routes/ClientHome/Login/index.tsx
+++ frontend/src/routes/ClientHome/Login/index.tsx
@@ -36,7 +36,7 @@ export  default function Login() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        authService.loginRequest({ username: formData.username.value, password: formData.password.value })
+        authService.loginRequest(forms.toValues(formData))
             .then(response => {
                 authService.saveAccessToken(response.data.access_token);
                 setContextTokenPayload(authService.getAccessTokenPayload());
--- frontend/src/utils/forms.ts
+++ frontend/src/utils/forms.ts
@@ -1,3 +1,11 @@
 export function update(inputs: any, name: string, newValue: any) {
     return { ...inputs, [name]: {...inputs[name], value: newValue } };
 }
+
+export function toValues(inputs: any) {
+    const data: any = {};
+    for(var name in inputs) {
+        data[name] = inputs[name].value;
+    }
+    return data;
+}
