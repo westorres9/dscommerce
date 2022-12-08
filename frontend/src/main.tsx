66761caa1d0af9e882c8f1ce9c05e9b3ce14d844
westorres9 <wester.torres9@gmail.com>
2022-12-08 15:49:49 -0300

    ready to deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+window.React = React
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
 )
