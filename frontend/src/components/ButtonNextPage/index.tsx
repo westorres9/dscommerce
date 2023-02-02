3d9d4cb1394bcda66ae9021ab50f786221e5ca7c
westorres9 <wester.torres9@gmail.com>
2023-02-02 00:03:19 -0300

    frontend started

--- frontend/src/components/ButtonNextPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-export default function ButtonNextPage() {
-    return (
-        <div className="dsc-btn-next-page">
-            Carregar mais
-        </div>
-    );
-}
--- frontend/src/utils/system.ts
+++
@@ -1,9 +0,0 @@
-export const CART_KEY = "com.devsuperior.dscommerce/cart";
-
-export const TOKEN_KEY = "com.devsuperior.dscommerce/token";
-
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
-
-export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
-
-export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
